bba7656c2d32f15127cde3ddb37a19e3bdfc7a95
Shereyton <shereytonblocker1@gmail.com>
2025-07-31 22:32:08 -0400

    deleted

--- logging.test.js
+++ logging.test.js
@@ -1,9 +1,1 @@
-const { canLogSet } = require('./script');
 
-test('allows weight zero', () => {
-  expect(canLogSet(0, 5)).toBe(true);
-});
-
-test('rejects invalid reps', () => {
-  expect(canLogSet(0, 0)).toBe(false);
-});
