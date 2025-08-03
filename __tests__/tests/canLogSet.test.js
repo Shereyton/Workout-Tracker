f91bc53a192bbba06cd45dfe0b52cf5e535dba19
Shereyton <shereytonblocker1@gmail.com>
2025-08-02 21:18:46 -0400

    update canLogSet.test.js

---
+++ __tests__/tests/canLogSet.test.js
@@ -0,0 +1,10 @@
+const { canLogSet } = require('../script');
+
+describe('canLogSet', () => {
+  it('allows zero weight with positive reps', () => {
+    expect(canLogSet(0, 5)).toBe(true);
+  });
+  it('rejects invalid reps', () => {
+    expect(canLogSet(50, 0)).toBe(false);
+  });
+});
